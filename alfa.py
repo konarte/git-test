64b80d877e5cdd60e740c0b020d29f2b9f9e11ac
konart <konart@gmail.com>
2022-03-16 17:54:39 +0300

    новые файлы 2

---
+++ alfa.py
@@ -0,0 +1,21 @@
+import sys
+import time
+
+'''
+alfa = []
+#list(alfa)
+#1-1000
+i = 1
+while i < 101:
+	alfa.append(i)
+	i += 1 # i=i+1
+
+print(alfa)
+'''
+
+print("=================================")
+for i in range (1, 100, 3):
+	print( i, end=" ")
+
+#x=input('___')
+time.sleep(5)
---
+++ beta.py
@@ -0,0 +1,9 @@
+import sys
+
+f = open('"D:\\projects\\git-test\\dir.txt','r')
+f.read()
+for line in f:
+    print(line)
+
+    
+
